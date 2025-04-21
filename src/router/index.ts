6745598612a7ac3a8077b019511cb17d056788d6
yo <yo@luciditv.com>
2025-04-21 12:05:20 +0800

    feat: add agora video call

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/SendBird/GroupCallView.vue'),
     },
+    {
+      path: '/agora/videoCall',
+      name: 'agoraVideoCall',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/Agora/VideoCall.vue'),
+    },
   ],
 })
 
---
+++ src/views/Agora/VideoCall.vue
@@ -0,0 +1,111 @@
+<script lang="ts" setup>
+import AgoraRTC, {
+  type IAgoraRTCClient,
+  type ILocalAudioTrack,
+  type ILocalVideoTrack,
+} from 'agora-rtc-sdk-ng'
+import { onMounted, ref } from 'vue'
+
+let agoraClient: IAgoraRTCClient | null = null
+const this_appId = ref(import.meta.env.VITE_AGORA_APP_ID ?? '')
+const this_channel = ref(import.meta.env.VITE_AGORA_CHANNEL ?? '')
+const this_token = ref(import.meta.env.VITE_AGORA_TOKEN ?? '')
+const this_uid = ref(import.meta.env.VITE_AGORA_UID ?? '')
+
+function agoraInit() {
+  agoraClient = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' })
+  setupEventListeners()
+}
+
+agoraInit()
+function setupEventListeners() {
+  // Declare event handler for "user-published"
+  agoraClient?.on('user-published', async (user, mediaType) => {
+    // Subscribe to media streams
+    await agoraClient?.subscribe(user, mediaType)
+    if (mediaType === 'video') {
+      // Specify the ID of the DOM element or pass a DOM object.
+      user.videoTrack?.play('<Specify a DOM element>')
+    }
+    if (mediaType === 'audio') {
+      user.audioTrack?.play()
+    }
+  })
+
+  // Handle the "user-unpublished" event to unsubscribe from the user's media tracks
+  agoraClient?.on('user-unpublished', async (user) => {
+    const remotePlayerContainer = document.getElementById(user.uid.toString())
+    remotePlayerContainer?.remove()
+  })
+}
+
+// Declare variables for local tracks
+let localAudioTrack: ILocalAudioTrack | null = null
+let localVideoTrack: ILocalVideoTrack | null = null
+
+// Create local audio and video tracks
+async function createLocalMediaTracks() {
+  localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack()
+  localVideoTrack = await AgoraRTC.createCameraVideoTrack()
+}
+
+function displayLocalVideo() {
+  const localPlayerContainer = document.getElementById(this_uid.value) as HTMLDivElement
+  if (localVideoTrack) localVideoTrack?.play(localPlayerContainer)
+}
+
+async function publishLocalTracks() {
+  const tracks = []
+  if (localAudioTrack) tracks.push(localAudioTrack)
+  if (localVideoTrack) tracks.push(localVideoTrack)
+  await agoraClient?.publish(tracks)
+}
+
+async function joinChannel() {
+  await agoraClient?.join(this_appId.value, this_channel.value, this_token.value, this_uid.value)
+  await createLocalMediaTracks()
+  await publishLocalTracks()
+}
+
+async function leaveChannel() {
+  // Stop the local media tracks to release the microphone and camera resources
+  if (localAudioTrack) {
+    localAudioTrack.close()
+    localAudioTrack = null
+  }
+  if (localVideoTrack) {
+    localVideoTrack.close()
+    localVideoTrack = null
+  }
+  // Leave the channel
+  await agoraClient?.leave()
+}
+
+onMounted(() => {})
+</script>
+<template>
+  <div>Agora VideoCall</div>
+  <div>
+    <div>
+      <span>你的appId</span>
+      <input v-model="this_appId" />
+    </div>
+    <div>
+      <span>你的channel</span>
+      <input v-model="this_channel" />
+    </div>
+    <div>
+      <span>你的token</span>
+      <input v-model="this_token" />
+    </div>
+    <div>
+      <span>你的uid</span>
+      <input v-model="this_uid" />
+    </div>
+    <button @click="joinChannel()">join channel</button>
+    <button @click="leaveChannel()">leave channel</button>
+  </div>
+  <div>
+    <div :id="this_uid">{{ `Local User: ${this_uid}` }}</div>
+  </div>
+</template>
